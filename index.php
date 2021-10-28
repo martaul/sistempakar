89888d747d4d18aeca6f97ce8667d44cce8d584c
Martaulina <70958028+martaul@users.noreply.github.com>
2021-10-28 10:13:03 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="website seleksi jurusan kuliah">
+    <link rel="icon" href="image/icon.png">
+
+    <title>Sistem Pakar Pemilihan Jurusan</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/carousel.css" rel="stylesheet">
+</head>
+<body>
+	<?php
+		include ('navbar.php');
+	?>
+	
+    <main role="main">
+        <!-- Carousel -->
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+          <li data-target="#myCarousel" data-slide-to="1"></li>
+          <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="first-slide" src="image/slide1.jpg" alt="First slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h1>Salah jurusan bisa menghambat<br> <span>Mahasiswa untuk lulus.</span></h1>
+                <p>Berdasarkan data penelitian Mahasiswa pertahun</p>
+                <p><button type="button" class="btn btn-lg btn-info" data-target="#exampleModal" data-toggle="modal" data-whatever="@getbootstrap">Mulai</button></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="second-slide" src="image/slide2.jpg" alt="Second slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h1>Gali minat dan bakat mu<br>Mulai dari sekarang</h1>
+                <p>Masih bingung dengan minat dan bakatmu?</p>
+                <p><button type="button" class="btn btn-lg btn-info" data-target="#exampleModal" data-toggle="modal" data-whatever="@getbootstrap">Mulai</button></p>
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="third-slide" src="image/slide3.jpg" alt="Third slide">
+            <div class="container">
+              <div class="carousel-caption text-left">
+                <h1>Siswa di Indonesia masiih banyak mengalami <br> <span>Salah jurusan.</span></h1>
+                <p>Berdasarkan data penelitian Mahasiswa pertahun</p>
+                <p><button type="button" class="btn btn-lg btn-info" data-target="#exampleModal" data-toggle="modal" data-whatever="@getbootstrap">Mulai</button></p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div> 
+      <!-- Carousel -->
+
+      <!---------------- MODAL ----------------->
+      <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-sm" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Identitas User</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form action="simpan-session.php" method="post" enctype="multipart/form-data" role="form">
+            <div class="modal-body">
+                <div class="form-group">
+                  <label for="recipient-name" class="col-form-label">Nama :</label>
+                  <input type="text" name="nama" class="form-control" id="input-ame" placeholder="isikan nama anda" required autofocus >
+                </div>
+                <div class="form-group">
+                  <label for="message-text" class="col-form-label">Umur :</label>
+                  <input type="number" name="umur" class="form-control col-sm-8" placeholder="isikan umur anda" required >
+                </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="submit" class="btn btn-info">Lanjut ></button>
+                </div>
+              </form>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+      <!-- Marketing messaging and featurettes
+      ================================================== -->
+      <!-- Wrap the rest of the page in another container to center all the content. -->
+
+      <div class="container marketing">
+
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="image/martah.jpg" alt="Generic placeholder image" width="140" height="140">
+            <h2>Martaulina</h2>
+            <p>Mahasiswi Informatika 2019</p>
+            <p>201910225229</p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="image/tiarah.jpg" alt="Generic placeholder image" width="140" height="140">
+            <h2>Tiara Nur Fadilah</h2>
+            <p>Mahasiswi Informatika 2019</p>
+            <p>201910225281</p>
+          </div><!-- /.col-lg-4 -->
+          <div class="col-lg-4">
+            <img class="rounded-circle" src="image/peni.jpg" alt="Generic placeholder image" width="140" height="140">
+            <h2>Fenny Ihrom Wulandari</h2>
+            <p>Mahasiswi Informatika 2019</p>
+            <p>201910225369</p>
+          </div><!-- /.col-lg-4 -->
+        </div><!-- /.row -->
+
+
+
+    </main>
+
+	<!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+</html>
